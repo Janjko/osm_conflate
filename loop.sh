712ff9c9ade1dd1760f505ea97a995d89388ed46
Janko Mihelić <janko.mihelic@adnet.hr>
2022-01-16 00:42:55 +0100

    Added dynamic folder creation

--- loop.sh
+++ loop.sh
@@ -13,18 +13,14 @@ function sigterm() {
 trap 'sigint' INT
 trap 'sigterm' TERM
 
-mkdir -p /jsons/current/
-mkdir -p /jsons/inspected/
-mkdir -p /jsons/history/
-
-mkdir -p /jsons/poste/current/
-mkdir -p /jsons/poste/inspected/
-mkdir -p /jsons/poste/history/
-
 while ${run}; do
   for i in /data/*.py; do
     [ -f "$i" ] || break
 	filename=$(basename -- "$i")
+	mkdir -p /jsons/"${filename%.*}"/current/
+    mkdir -p /jsons/"${filename%.*}"/inspected/
+    mkdir -p /jsons/"${filename%.*}"/history/
+	mkdir -p /rss/"${filename%.*}"/
 	echo  $filename
     conflate $i -o /data/josm.osm --changes /jsons/"${filename%.*}"/current/changes.json --list_duplicates
 	python3 conflate2rss/conflate2rss.py -n /jsons/"${filename%.*}"/current/changes.json -i /jsons/"${filename%.*}"/inspected/changes.json -r /rss/"${filename%.*}"/rss.xml -w /rss/"${filename%.*}"/raw_rss.json -p /jsons/"${filename%.*}"/history/ --rssurl http://46.101.158.129:8080/rss/"${filename%.*}"/rss.xml --rssauthor Janjko --rsslanguage hr --number_of_entries 10 --title /"${filename%.*}"
