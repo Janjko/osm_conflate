16985caed8283d237f777cf2ec18f7c80abc3d19
Janko Mihelić <janko.mihelic@adnet.hr>
2022-01-16 00:37:26 +0100

    Wrong variable in wrong place

--- loop.sh
+++ loop.sh
@@ -26,7 +26,7 @@ while ${run}; do
     [ -f "$i" ] || break
 	filename=$(basename -- "$i")
 	echo  $filename
-    conflate $filename -o /data/josm.osm --changes /jsons/"${filename%.*}"/current/changes.json --list_duplicates
+    conflate $i -o /data/josm.osm --changes /jsons/"${filename%.*}"/current/changes.json --list_duplicates
 	python3 conflate2rss/conflate2rss.py -n /jsons/"${filename%.*}"/current/changes.json -i /jsons/"${filename%.*}"/inspected/changes.json -r /rss/"${filename%.*}"/rss.xml -w /rss/"${filename%.*}"/raw_rss.json -p /jsons/"${filename%.*}"/history/ --rssurl http://46.101.158.129:8080/rss/"${filename%.*}"/rss.xml --rssauthor Janjko --rsslanguage hr --number_of_entries 10 --title /"${filename%.*}"
 	sleep ${PERIOD:-24h}
   done
